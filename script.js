1fd9d153c41643256897c6304eb7a1bef2d2097b
Nick Wong <nicolaswong01@gmail.com>
2022-07-17 16:54:35 -0700

    Ensure the nav links change color if used from title section

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// resizing of header when pinned at top
+// dynamic header based on window position
 ScrollTrigger.create({
     trigger: '.nav',
     start: 'top top',
     end: 99999,
     toggleClass: {className: 'pinned', targets: '.nav'},
 });
-  
+
 ScrollTrigger.create({
-    trigger: '#content',
-    start: 'top top',
-    end: 'bottom bottom',
+    trigger: '#title',
+    start: 'bottom 1%',
+    endTrigger: '#content',
+    end: 'bottom 99%',
     toggleClass: {className: 'pinned-link', targets: '.link'},
     markers: true,
+});
+
+const links = document.querySelectorAll('.link');
+links.forEach((link, i) => {link.addEventListener('click', () => {
+        console.log("clicked");
+        links.forEach((link, i) => {link.classList.add('pinned-link')});
+    });
 });
