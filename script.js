9f6114a95f4e8256fd391bad649164dd15ee9d07
Nick Wong <nicolaswong01@gmail.com>
2022-07-20 13:00:29 -0700

    Add footer section

--- script.js
+++ script.js
@@ -18,17 +18,44 @@ ScrollTrigger.create({
 ScrollTrigger.create({
     trigger: '#title',
     start: 'bottom 1%',
-    endTrigger: '#content',
+    endTrigger: '#footer',
     end: 'bottom 99%',
     toggleClass: {className: 'pinned-link', targets: '.link'},
 });
 
 // highlights current section in nav
-document.querySelectorAll('section:not(#title)').forEach((section, i) => {
-    ScrollTrigger.create({
-        trigger: section,
-        start: 'top 1%',
-        end: 'bottom top',
-        toggleClass: {className: 'current', targets: `a[href=\'#${section.id}\']`},
-    })
-})
+// document.querySelectorAll('section:not(#title)').forEach((section, i) => {
+//     ScrollTrigger.create({
+//         trigger: section,
+//         start: 'top 1%',
+//         end: 'bottom top',
+//         toggleClass: {className: 'current', targets: `a[href=\'#${section.id}\']`},
+//     })
+// });
+
+// projects
+ScrollTrigger.create({
+    trigger: '#content',
+    start: 'top top',
+    endTrigger: '#about',
+    end: 'bottom 90%',
+    toggleClass: {className: 'current', targets: 'a[href=\'#projects\']'},
+    // markers: true,
+});
+
+// about
+ScrollTrigger.create({
+    trigger: '#about',
+    start: 'bottom 90%',
+    endTrigger: '#footer',
+    end: 'bottom bottom',
+    toggleClass: {className: 'current', targets: 'a[href=\'#about\']'},
+    // markers: true,
+});
+
+// contact
+ScrollTrigger.create({
+    trigger: '#contact',
+    start: 'top 31%',
+    toggleClass: {className: 'current', targets: 'a[href=\'#contact\']'},
+});
