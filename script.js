4c44b325e860651009f51d8dcf0264fd0ae8f4c7
Nick Wong <nicolaswong01@gmail.com>
2022-07-28 19:58:56 -0700

    Clean up code

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ gsap.registerPlugin(ScrollTrigger);
 
 // dynamic header based on window position
 ScrollTrigger.create({
-    trigger: '.nav',
+    trigger: '.header',
     start: 'top top',
     end: 99999,
-    toggleClass: {className: 'pinned', targets: '.nav'},
+    toggleClass: {className: 'pinned', targets: '.header'},
 });
 
 ScrollTrigger.create({
-    trigger: '#projects',
+    trigger: 'section#projects',
     start: 'top top',
     end: 99999,
-    toggleClass: {className: 'opaque', targets: '.nav'},
+    toggleClass: {className: 'opaque', targets: '.header'},
 });
 
 ScrollTrigger.create({
-    trigger: '#title',
+    trigger: 'section#title',
     start: 'bottom 1%',
     endTrigger: '#footer',
     end: 'top 70%',
-    toggleClass: {className: 'pinned-link', targets: '.link'},
+    toggleClass: {className: 'pinned-link', targets: '.header > .link'},
 });
 
 // ScrollTo animations
-document.querySelectorAll('.link').forEach(link => {
+document.querySelectorAll('.header > .link').forEach(link => {
     link.addEventListener('click', () => {
         gsap.to(window, {scrollTo: `#${link.textContent}`});    
     })
